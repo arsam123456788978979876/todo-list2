e0244f094724e6d9ff1eabe7d93fa5b0b3aa1228
arsam123456788978979876 <arsamsarkhosh4@gmail.com>
2023-10-24 18:06:36 +0330

    sdf

--- assets/index.js
+++ assets/index.js
@@ -11,68 +11,75 @@ function renderItem(val) {
 
     const p = document.createElement("p");
     p.textContent = val
+
+    let del = document.createElement("span")
+    del.textContent = "x"
     
     const checkbox = document.createElement("input");
    checkbox.setAttribute("type" , "checkbox")
 
    div.appendChild(checkbox)
    div.appendChild(p)
+   div.appendChild(del)
 
    add.appendChild(div)
-   
+    dele(del , div)
+}
+
+function dele(del , div) {
+    del.addEventListener("click" , () => {
+        todo_list.splice(0 , 1)
+        console.log(todo_list)
+        div.remove()
+        localStorage.removeItem("todo")
+    })
 }
 
 
-let clearInput = () => {
+
+function clearInput() {
     input.value = ""
 }
+
+function setItemToStorage(val) {
+    todo_list.push(val)
+    let obj = JSON.stringify(todo_list);
+    localStorage.setItem("todo" , obj);
+}
+function loadFromStorage() {
+    let load = JSON.parse(localStorage.getItem("todo")) || [];
+    todo_list = load
+}
+
 function looping() {
-    for(let i; i<todo_list.length; i++) {
-        let item = todo_list[i]
-        renderItem(item)
-    }
-}
-function setItemToStorage(value) {
-      todo_list.push(value);
-       let obj =  JSON.stringify(todo_list)
-       console.log(obj)
-       localStorage.setItem("f" , todo_list)
-}
-function getItemFromStorage() {
-    let a = localStorage.getItem("f");
-   let b =  JSON.parse(a) || [];
-    
-    todo_list = b
+    todo_list.forEach((element) => {
+        renderItem(element)
+    })
 }
 
-
-
-function event() {
-    
+function events() {
     enterAction.addEventListener("submit" , (e) => {
-        let value = input.value
+        let val = input.value
         e.preventDefault()
-        if(input.value==""){
-                    button.classList.add("error")
-                alert("your input is empty")
-            }else {
-                input.addEventListener("keydown" , () => {
-                    button.classList.remove("error")
-                })
-                    setItemToStorage(value)
-                    renderItem(value)
-                    clearInput()
-                    
-            }
-        })
+
+        if(input.value == "") {
+            button.classList.add("error")
+            alert("heyyyyyyyyyyyyyyyyy")
+        }else {
+            input.addEventListener("keydown" , () => {
+                button.classList.remove("error")
+            })
+            renderItem(val)
+            clearInput()
+            setItemToStorage(val)
+
+        }
+        console.log(todo_list)
+    })
 }
-
-
-function init () {
-    getItemFromStorage()
+function init() {
+    loadFromStorage()
     looping()
-    event()
+    events()
 }
 init()
-
-
